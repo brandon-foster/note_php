3006073f3e3230bebf12c0c27196b5fff4c22e8a
Brandon Foster <brandon.foster.vt@gmail.com>
2015-03-11 15:14:33 -0400

    use absolute urls for syntax highlighter in post html

--- view/post-html.php
+++ view/post-html.php
@@ -10,11 +10,11 @@ $categoryNameLower = strtolower($categoryName);
 $categoryNameDashed = StringFunctions::spaceToDash($categoryNameLower);
 $pageData->setBodyClass("body-{$categoryNameDashed}");
 
-$pageData->addCss('res/syntax-highlighter/styles/shCore.css');
-$pageData->addCss('res/syntax-highlighter/styles/shThemeDefault.css');
-$pageData->addJs('res/syntax-highlighter/scripts/shCore.js');
-$pageData->addJs('res/syntax-highlighter/scripts/shBrushJScript.js');
-$pageData->addJs('res/syntax-highlighter/scripts/shBrushBash.js');
+$pageData->addCss('/res/syntax-highlighter/styles/shCore.css');
+$pageData->addCss('/res/syntax-highlighter/styles/shThemeDefault.css');
+$pageData->addJs('/res/syntax-highlighter/scripts/shCore.js');
+$pageData->addJs('/res/syntax-highlighter/scripts/shBrushJScript.js');
+$pageData->addJs('/res/syntax-highlighter/scripts/shBrushBash.js');
 $pageData->addScriptCode('SyntaxHighlighter.all();');
 
 $out = "
